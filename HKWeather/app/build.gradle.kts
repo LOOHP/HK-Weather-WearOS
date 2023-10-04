7ca2e27e2b4e7e9ae87427143c434c7f5cf79e8d
LOOHP <jamesloohp@gmail.com>
2023-10-05 00:08:42 +0100

    Order weather warnings as on the HKO website

--- HKWeather/app/build.gradle.kts
+++ HKWeather/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.loohp.hkweatherwarnings"
         minSdk = 30
         targetSdk = 33
-        versionCode = 72
+        versionCode = 73
         versionName = "1.3.1"
     }
 
