69cc7f918572d4f87ccb1cfc8acb68c1688a8149
LOOHP <jamesloohp@gmail.com>
2023-11-22 18:16:49 +0000

    Dynamic Shaped Icon

--- HKWeatherPhoneCompanion/app/build.gradle.kts
+++ HKWeatherPhoneCompanion/app/build.gradle.kts
@@ -25,14 +25,14 @@ plugins {
 
 android {
     namespace = "com.loohp.hkweatherwarnings"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.loohp.hkweatherwarnings"
         minSdk = 24
         targetSdk = 33
-        versionCode = 92
-        versionName = "1.3.5"
+        versionCode = 107
+        versionName = "1.3.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -78,9 +78,9 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
